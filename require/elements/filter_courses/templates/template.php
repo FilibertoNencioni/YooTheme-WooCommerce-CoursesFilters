1fc0a89d81816e9a08d5fabf41009e462a0e7352
FilibertoNencioni <filiberto.nencioni02@gmail.com>
2022-04-04 14:09:43 +0200

    test

--- require/elements/filter_courses/templates/template.php
+++ require/elements/filter_courses/templates/template.php
@@ -40,7 +40,8 @@ $container = $this->el('div', [
 $filterContainer = $this->el('div',[
     'class'=>[
         'uk-flex-first',
-        'uk-width-1-4@m'
+        'uk-width-1-4@m',
+        'filters'
     ]
 ]);
 
@@ -76,18 +77,21 @@ $table = $this->el('table', [
 
 ]);
 
-
+function printAttrTags($attributes){
+    $tags = "";
+    for($i = 0; $i<count($attributes);++$i){
+        $index = trim(array_keys($attributes)[$i]);
+        $tags.= " tag-".preg_replace('/\s+/', '-', $index)."='".$attributes[$index][0]."'";
+    }
+    echo $tags;
+}
 
 ?>
 
-<!-- <?php if ($props['table_responsive'] == 'overflow') : ?> -->
     <?= $container ?>
     <?= $el($props, $attrs) ?>
     <?= $table($props) ?>
-<!-- <?php else : ?>
-    <?= $grid ?>
-    <?= $table($props, $attrs) ?>
-<?php endif ?> -->
+
 
         <?php if (Arr::some($filtered, function ($field) use ($props) { return $props["table_head_{$field}"]; })) : ?>
         <thead>
@@ -137,9 +141,13 @@ $table = $this->el('table', [
 
                 ?>
                 <?php if(!Str::length($link)) : ?>
-                    <tr class="el-item" tag-tecnologie="<?= $singleAttributes['Tecnologie'][0]?>" tag-ruolo="<?= $singleAttributes['Ruolo'][0]?>" tag-vendor="<?= $singleAttributes['Vendor'][0]?>" tag-erogazione="<?= $singleAttributes['Modalita di erogazione'][0]?>" tag-durata="<?= $singleAttributes['Durata corso'][0]?>" tag-calendario="<?= $singleAttributes['Calendario'][0]?>" tag-sede="<?= $singleAttributes['Sede'][0]?>" tag-status="<?= $singleAttributes['Status'][0]?>"><?= $builder->render($child, ['i' => $i, 'element' => $props, 'fields' => $fields, 'text_fields' => $text_fields, 'filtered' => $filtered]) ?></tr>
+                    <tr class="el-item corso" id="corso-<?= $i ?>" <?= printAttrTags($singleAttributes) ?>><?= $builder->render($child, ['i' => $i, 'element' => $props, 'fields' => $fields, 'text_fields' => $text_fields, 'filtered' => $filtered]) ?></tr>
+
+                    <!-- <tr class="el-item" tag-tecnologie="<?= $singleAttributes['Tecnologie'][0]?>" tag-ruolo="<?= $singleAttributes['Ruolo'][0]?>" tag-vendor="<?= $singleAttributes['Vendor'][0]?>" tag-erogazione="<?= $singleAttributes['Modalita di erogazione'][0]?>" tag-durata="<?= $singleAttributes['Durata corso'][0]?>" tag-calendario="<?= $singleAttributes['Calendario'][0]?>" tag-sede="<?= $singleAttributes['Sede'][0]?>" tag-status="<?= $singleAttributes['Status'][0]?>"><?= $builder->render($child, ['i' => $i, 'element' => $props, 'fields' => $fields, 'text_fields' => $text_fields, 'filtered' => $filtered]) ?></tr> -->
                 <?php else : ?>
-                    <tr class="el-item" tag-tecnologie="<?= $singleAttributes['Tecnologie'][0]?>" tag-ruolo="<?= $singleAttributes['Ruolo'][0]?>" tag-vendor="<?= $singleAttributes['Vendor'][0]?>" tag-erogazione="<?= $singleAttributes['Modalita di erogazione'][0]?>" tag-durata="<?= $singleAttributes['Durata corso'][0]?>" tag-calendario="<?= $singleAttributes['Calendario'][0]?>" tag-sede="<?= $singleAttributes['Sede'][0]?>" tag-status="<?= $singleAttributes['Status'][0]?>" style="cursor:pointer;" onclick="window.location='<?= $link ?>'"><?= $builder->render($child, ['i' => $i, 'element' => $props, 'fields' => $fields, 'text_fields' => $text_fields, 'filtered' => $filtered]) ?></tr>
+                    <tr class="el-item corso" id="corso-<?= $i ?>" <?= printAttrTags($singleAttributes) ?> style="cursor:pointer;" onclick="window.location='<?= $link ?>'"><?= $builder->render($child, ['i' => $i, 'element' => $props, 'fields' => $fields, 'text_fields' => $text_fields, 'filtered' => $filtered]) ?></tr>
+
+                    <!-- <tr class="el-item" tag-tecnologie="<?= $singleAttributes['Tecnologie'][0]?>" tag-ruolo="<?= $singleAttributes['Ruolo'][0]?>" tag-vendor="<?= $singleAttributes['Vendor'][0]?>" tag-erogazione="<?= $singleAttributes['Modalita di erogazione'][0]?>" tag-durata="<?= $singleAttributes['Durata corso'][0]?>" tag-calendario="<?= $singleAttributes['Calendario'][0]?>" tag-sede="<?= $singleAttributes['Sede'][0]?>" tag-status="<?= $singleAttributes['Status'][0]?>" style="cursor:pointer;" onclick="window.location='<?= $link ?>'"><?= $builder->render($child, ['i' => $i, 'element' => $props, 'fields' => $fields, 'text_fields' => $text_fields, 'filtered' => $filtered]) ?></tr> -->
                 <?php endif ?>
             <?php endforeach ?>
 
@@ -174,13 +182,14 @@ $table = $this->el('table', [
 
         <div class="uk-card uk-card-body uk-card-default uk-padding-small">
             <h3 class="uk-h5"><?= $index ?></h3>
-            <div class="uk-flex uk-flex-column">
+            <div class="uk-flex uk-flex-column filters-<?= strtolower(preg_replace('/\s+/', '-', $index)) ?>">
             <?php foreach($attributes[$index] as $attr) :
                 $attr = trim($attr); 
-                $tagvalue="tag-".preg_replace('/\s+/', '-', $index)."-value-".preg_replace('/\s+/', '-', $attr);
+                // $tagvalue="tag-".preg_replace('/\s+/', '-', $index)."-value-".preg_replace('/\s+/', '-', $attr);
+                $tagvalue ='tag-'.preg_replace('/\s+/', '-', $index).'="'.preg_replace('/\s+/', '-', $attr).'"';
                 ?>
 
-                <label><input class="uk-checkbox" type="checkbox" value="<?= $tagvalue?>"> <?= $attr ?> </label>
+                <label><input class="uk-checkbox" type="checkbox" <?= $tagvalue?>> <?= $attr ?> </label>
             <?php endforeach ?>
             <!-- Settare onClick (passa come attributo 'nomeattributo'-'termine' es: tecnologie-cloud oppure ruolo-cloud-engineer) -->
             </div>       
@@ -190,3 +199,77 @@ $table = $this->el('table', [
     <?php endfor ?>
     </div>
 <?php endif ?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
+<script type="text/javascript">
+    var classNames = [];
+    $('div[class*="filters-"]').each(function(i, el){
+        var name = (el.className.match(/(^|\s)(filters\-[^\s]*)/) || [,,''])[2];
+        if(name){
+            classNames.push(name);
+        }
+    });
+
+    $(".filters :checkbox").click(function() {
+        //Populating the array with all the courses, this is used to see which courses match the attributes
+        const corsiDaMostrare = [];
+        const initCorsi = Object.keys($(".corso")).map(function (key) {return $(".corso")[key];}).slice(0,-2);
+
+        for(let i = 0; i < initCorsi.length; i++){
+            corsiDaMostrare.push({corso: initCorsi[i], ok: true});
+        }
+
+        $(".filters :checkbox:checked").each(function() {
+            //Check match of attributes
+            for (let i = 0; i < classNames.length; i++) {
+                let tag = classNames[i].replace('filters','tag');
+                // console.log("searching by tag: "+tag);
+                let tagValue = $(this).attr(tag);
+                // if(tagValue){
+                //     console.log("tagvalue: "+tagValue);
+                // }
+
+                if(tagValue){
+                    $(".corso").each(function(){
+                        let attrFound = false;
+
+                        if($(this).attr(tag)){
+                            const selectedCorso = $(this);
+                            // console.log({title:"selected corso", selectedCorso});
+                            let corsoTagValues = selectedCorso.attr(tag).split(",");
+                            for(let j = 0; j<corsoTagValues.length;j++){
+                                if(tagValue === corsoTagValues[j].trim()){
+                                    attrFound=true;
+                                }
+                            }
+
+                            if(!attrFound){
+                                // console.log("inizio find in !attrFound");
+
+                                let corsoIndex = corsiDaMostrare.findIndex(function(elem){
+                                    if(elem.corso === selectedCorso[0]){
+                                        return elem;
+                                    }
+                                });
+                                if(corsoIndex !== -1){
+                                    corsiDaMostrare[corsoIndex].ok = false;
+                                }else{
+                                    console.log("ERRORE - INDICE IN RICERCA NON TROVATO")
+                                }
+                            }
+                        }
+
+                    })
+                }
+            }
+
+        });
+
+        for(let i = 0; i < corsiDaMostrare.length; i++){
+            if(corsiDaMostrare[i].ok === false){
+            }else{
+            }
+        }
+    });
+</script>
