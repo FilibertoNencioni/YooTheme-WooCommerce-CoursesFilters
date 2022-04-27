0af1b5774ddeac52213da63a61c3c22592c481c6
FilibertoNencioni <filiberto.nencioni02@gmail.com>
2022-04-27 22:43:25 +0200

    Dynamical product name and dates for editions

--- require/elements/edition_list/templates/template.php
+++ require/elements/edition_list/templates/template.php
@@ -1,38 +1,60 @@
 <?php
+$editions = [];
+$productID = wc_get_product()->get_id();
 
-$el = $this->el("ul", [
+$url = "http://one.wordpress.test/wp-json/wp/v2/date";
 
-    'class' => [
-        'uk-list',
-        'uk-margin-remove {position: absolute}',
-    ],
+$curl = curl_init($url);
+curl_setopt($curl, CURLOPT_URL, $url);
+curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 
-]);
+//for debug only!
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 
-$item = $this->el('li', [
+$resp = curl_exec($curl);
+curl_close($curl);
+$dates = json_decode($resp);
 
+foreach($dates as $date){
+    if($date->acf->corso_erogato == $productID){
+        $edition = new stdClass();
+        $edition->site = $date->acf->sede;
+
+        //SETTING DATA FORMAT
+        $dateOnly = $date->acf->data;
+        $formattedDate = sprintf("%s/%s/%s", substr($dateOnly, -2), substr($dateOnly, 4,2), substr($dateOnly,0, 4));
+
+        $edition->date = $formattedDate;
+        $editions[]=$edition;
+    }
+}
+
+$el = $this->el("div", [
     'class' => [
         'el-item',
     ],
-
 ]);
+
+$title = $this->el($props['title-style'],[]);
 
 ?>
 
-<?= $el($props, $attrs) ?>
-<?php foreach ($children as $i => $child) : ?>
+<?php if(count($editions)>0) : ?>
+    <?= $el($attrs) ?>
+        <?= $title ?> <?= $props['title'] ?> <?= $title->end() ?>
+        <form>
+            <?php foreach ($editions as $key => $edition) : ?>
+                <p> <?= $edition->date ?> - <?= $edition->site ?> </p>
+            <?php endforeach ?>
+        </form>
+    <?= $el->end() ?>
+<?php else : ?>
+    <?= $title ?> <?= $props['no-edition-title'] ?> <?= $title->end() ?>
+<?php endif ?>
 
-    <?= $item($props) ?>
-        <?= $builder->render($child, ['element' => $props]) ?>
-    <?= $item->end() ?>
-
-<?php endforeach ?>
-<?= $el->end() ?>
-
-<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<!-- <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
 <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script> -->
 
-<script type="text/javascript">
-    console.log($(".product_title").text());
-</script>
+
--- require/elements/edition_list_item/templates/content.php
+++
@@ -1,7 +0,0 @@
-<?php if ($props['date'] && !$props['site']) : ?>
-    <div><?= $props['date'] ?></div>
-<?php elseif(!$props['date'] && $props['site']) : ?>
-    <div> <?= $props['site'] ?></div>
-<?php elseif($props['date'] && $props['site']) : ?>
-    <div><?= $props['date'] ?> - <?= $props['site'] ?></div>
-<?php endif ?>
