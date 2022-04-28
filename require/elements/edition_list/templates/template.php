0db2e1e98ef8f3cfc06dc61b5b6e4ba582b2c04d
FilibertoNencioni <filiberto.nencioni02@gmail.com>
2022-04-28 12:10:22 +0200

    Added interaction with contact form

--- require/elements/edition_list/templates/template.php
+++ require/elements/edition_list/templates/template.php
@@ -1,7 +1,9 @@
 <?php
 try {
     $productID = wc_get_product()->get_id();
-    $url = "http://one.wordpress.test/wp-json/wp/v2/date";
+    $productTitle = wc_get_product()->get_title();
+
+    $url = "http://one.wordpress.test/wp-json/wp/v2/date"; // CHANGE THIS WITH YOUR URL
     
     $curl = curl_init($url);
     curl_setopt($curl, CURLOPT_URL, $url);
@@ -20,7 +22,7 @@ try {
             $edition = new stdClass();
             $edition->site = $date->acf->sede;
     
-            //SETTING DATA FORMAT
+            //SETTING DATE FORMAT
             $dateOnly = $date->acf->data;
             $formattedDate = sprintf("%s/%s/%s", substr($dateOnly, -2), substr($dateOnly, 4,2), substr($dateOnly,0, 4));
     
@@ -56,12 +58,13 @@ $container = $this->el("div", [
 $title = $this->el($props['title-size'],[]);
 ?>
 
+<!-- DISPLAY LIST ITEM -->
 <?php if(count($editions)>0) : ?>
     <?= $container($attrs) ?>
         <?= $title ?> <?= $props['title'] ?> <?= $title->end() ?>
         <?= $form ?>
             <?php foreach ($editions as $key => $edition) : ?>
-                <label><input class="uk-radio" type="radio" name="edition" <?php if($key == 0) :?> checked <?php endif ?>> <?= $edition->date ?> - <?= $edition->site ?> </label>
+                <label><input class="uk-radio" type="radio" name="edition" value="<?= $edition->date ?> - <?= $edition->site ?>"<?php if($key == 0) :?> checked <?php endif ?>> <?= $edition->date ?> - <?= $edition->site ?> </label>
             <?php endforeach ?>
         <?= $form->end() ?>
     <?= $container->end() ?>
@@ -71,8 +74,28 @@ $title = $this->el($props['title-size'],[]);
     <?= $container->end() ?>
 <?php endif ?>
 
-<!-- <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
-<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
-<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script> -->
+
+<!-- CONTACT FORM INTERATION -->
+<?php if($props["use-cf7"] && $props['cf7-course'] != NULL && $props['cf7-edition'] != NULL) :?>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+    <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+    <script>
+        var courseNameID = <?= json_encode($props["cf7-course"]) ?>;
+        var editionID = <?= json_encode($props["cf7-edition"]) ?>;
+
+        $(document).ready(function(){
+            //SET INPUT AS DISABLED (USER CANNOT MODIFY VALUE)
+            $("."+courseNameID+" > input").attr("disabled",true);
+            $("."+editionID+" > input").attr("disabled",true);
+
+            $("."+courseNameID+" > input").val(<?= json_encode($productTitle )?>);
+            $("."+editionID+" > input").val($("input[name=edition]:checked").val());
+            $("input[type=radio][name=edition]").change(function(){
+                $("."+editionID+" > input").val($("input[name=edition]:checked").val());
+            });
+        });
+    </script>
+<?php endif ?>
 
 
